65975e11f51dd19fdea9bf095c928c5384154def
이상민 <smlee01@miridih.com>
2022-08-11 22:55:44 +0900

    refactor: improve test readability

--- module-watch-verifier/src/test/java/com/sangmin/watchverifier/domain/model/VideoTest.java
+++ module-watch-verifier/src/test/java/com/sangmin/watchverifier/domain/model/VideoTest.java
@@ -12,37 +12,25 @@ class VideoTest {
 
     @Test
     void can_verify_if_video_matches_given_attributes() {
-        // given
         var videoId = "test";
         var provider = Provider.YOUTUBE;
-        List<Property> properties = List.of(
-                new Category("여행"),
-                new Category("게임")
-        );
-
-        // when
+        List<Property> properties = List.of(new Category("여행"), new Category("게임"));
         Video sut = new Video(videoId, provider, properties);
+
         boolean matches = sut.matches(provider, properties.get(0));
 
-        // then
         assertTrue(matches);
     }
 
     @Test
     void can_verify_if_video_not_matches_given_attributes() {
-        // given
         var videoId = "test";
         var provider = Provider.YOUTUBE;
-        List<Property> properties = List.of(
-                new Category("여행"),
-                new Category("게임")
-        );
-
-        // when
+        List<Property> properties = List.of(new Category("여행"), new Category("게임"));
         Video sut = new Video(videoId, provider, properties);
+
         boolean matches = sut.matches(provider, new Category("문학"));
 
-        // then
         assertFalse(matches);
     }
 
--- module-watch-verifier/src/test/java/com/sangmin/watchverifier/domain/model/WatchFilterTest.java
+++ module-watch-verifier/src/test/java/com/sangmin/watchverifier/domain/model/WatchFilterTest.java
@@ -12,43 +12,21 @@ class WatchFilterTest {
 
     @Test
     void can_determine_video_matches_filter() {
-        // given
-        var video = new Video(
-                "test-id",
-                Provider.YOUTUBE,
-                List.of(new Category("test"))
-        );
+        var video = new Video("test-id", Provider.YOUTUBE, List.of(new Category("test")));
+        var sut = new WatchFilter(Provider.YOUTUBE, new Category("test"));
 
-        var sut = new WatchFilter(
-                Provider.YOUTUBE,
-                new Category("test")
-        );
-
-        // when
         var matches = sut.matches(video);
 
-        // then
         assertTrue(matches);
     }
 
     @Test
     void can_determine_video_not_matches_filter() {
-        // given
-        var video = new Video(
-                "test-id",
-                Provider.YOUTUBE,
-                List.of(new Category("other-category"))
-        );
+        var video = new Video("test-id", Provider.YOUTUBE, List.of(new Category("other-category")));
+        var sut = new WatchFilter(Provider.YOUTUBE, new Category("test"));
 
-        var sut = new WatchFilter(
-                Provider.YOUTUBE,
-                new Category("test")
-        );
-
-        // when
         var matches = sut.matches(video);
 
-        // then
         assertFalse(matches);
     }
 
