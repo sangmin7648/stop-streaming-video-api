baab604203976f7977f268a0b8f1a8b4d94069d9
이상민 <smlee01@miridih.com>
2022-08-11 19:26:27 +0900

    project setup

---
+++ module-watch-verifier/src/main/java/com/sangmin/watchverifier/WatchVerifierApplication.java
@@ -0,0 +1,13 @@
+package com.sangmin.watchverifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchVerifierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WatchVerifierApplication.class, args);
+    }
+
+}
---
+++ module-watch-verifier/src/test/java/com/sangmin/watchverifier/ModuleWatchVerifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sangmin.watchverifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleWatchVerifierApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
