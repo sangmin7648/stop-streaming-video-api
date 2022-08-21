34fa668e87768125d3706a535931b7c85095da40
Sangmin Lee <sangminlee7648@gmail.com>
2022-08-21 23:56:49 +0900

    feat: add register user api

--- module-user/src/main/java/com/sangmin/stopstreamingvideo/user/feature/RegisterUser.java
+++ module-user/src/main/java/com/sangmin/stopstreamingvideo/user/feature/RegisterUser.java
@@ -1,16 +1,41 @@
 package com.sangmin.stopstreamingvideo.user.feature;
 
+import an.awesome.pipelinr.Pipeline;
 import com.sangmin.stopstreamingvideo.common.Exceptions;
 import com.sangmin.stopstreamingvideo.shared.event.UserSignedUpEvent;
 import com.sangmin.stopstreamingvideo.user.domain.User;
 import com.sangmin.stopstreamingvideo.user.domain.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
 
 public class RegisterUser {
+
+    record Request(String username) { }
+
+    record Response(UUID userId) { }
+
+    @RestController
+    @RequiredArgsConstructor
+    static class Controller {
+
+        private final Pipeline pipeline;
+
+        @PostMapping("/v1/user")
+        ResponseEntity<Response> registerUser(@RequestBody Request request) {
+            var command = new Command(request.username());
+            UUID userId = command.execute(pipeline);
+
+            return ResponseEntity.ok(new Response(userId));
+        }
+
+    }
 
     public record Command(String username) implements an.awesome.pipelinr.Command<UUID> { }
 
